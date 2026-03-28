21a08c3922f8376820c969a85be5ee26b184a35b
koubonou <koubonou@users.noreply.github.com>
2026-03-28 09:28:30 -0400

    fix: remove analytics import from _app.js - already in index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,3 @@
-import { Analytics } from '@vercel/analytics/react';
-
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
